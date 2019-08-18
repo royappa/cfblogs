72c7bb971cf53904c753fff4c0097ec85ea8d852
royappa <royappa@spiralfx.com>
2019-08-18 14:26:10 -0500

    Connect to API, get recent actions, dedupe, display.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  blogEntries: any[];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.blogEntries = [];
+    const ids = [];
+    this.http.get('https://codeforces.com/api/recentActions?maxCount=100').subscribe(
+      data => { 
+          data['result'].forEach(e => {
+            const id = e.blogEntry.id;
+            const title = e.blogEntry.title;
+            if (!ids.includes(id)) {
+              this.blogEntries.push({id, title});
+              ids.push(id);
+            }
+          });
+        }
+    );
+
+
+  }
 }
